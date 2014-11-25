7865eaca27a1501b5744129b8dd46a2a2b4eb1c1
mama-egg <Kinseygoesrawr@gmail.com>
2014-11-25 08:17:56 -0700

    Update GroceryItem.java

--- src/GroceryItem.java
+++ src/GroceryItem.java
@@ -1,1 +1,29 @@
 // class to represent a grocery item
+public class GroceryItem {
+  
+  public GroceryItem(String name, int quantity, double PricePerUnit) { 
+    //Create an array of the item's name, quantity, and price per unit.
+    int[] itemorderarray;
+    itemorderarray = new int[3];
+    
+    public GroceryItem = new GroceryItem[3];
+    public String name;
+    public int quantity;
+    public double PricePerUnit;
+    
+    public double getCost() {
+      //Find the cost of the grocery item at its quantity and price per unit
+      double totalcost = quantity*PricePerUnit;
+      return totalcost;
+    }
+    
+    public void setQuantity(int quantity) {
+      //Set the quantity for some reason?????
+    }
+    
+    public String toString() {
+      String sgroceryitem = (quantity + " " + name + "s @ $" + PricePerUnit + " = $" + totalcost);
+      return sgroceryitem;
+    }
+  }
+}
